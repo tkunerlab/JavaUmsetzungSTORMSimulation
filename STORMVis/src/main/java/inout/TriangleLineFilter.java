1ff529a0cbb99977fcffcfb4c517d207775a191c
Sandro Braun <sandro.braun@iwr.uni-heidelberg.de>
2020-09-29 17:37:47 +0200

    formatting

--- STORMVis/src/main/java/inout/TriangleLineFilter.java
+++ STORMVis/src/main/java/inout/TriangleLineFilter.java
@@ -1,6 +1,5 @@
 package inout;
 
-
 import java.io.File;
 
 import javax.swing.filechooser.FileFilter;
@@ -9,9 +8,9 @@ public class TriangleLineFilter extends FileFilter {
 
 	@Override
 	public boolean accept(File f) {
-	    if (f.isDirectory()) {
-	        return true;
-	    }
+		if (f.isDirectory()) {
+			return true;
+		}
 
 		return Utils.accept(f);
 	}
@@ -21,27 +20,27 @@ public class TriangleLineFilter extends FileFilter {
 		// TODO Auto-generated method stub
 		return ".nff/.wimp/.ply/.txt";
 	}
-	
+
 	public static class Utils {
 
-	    public final static String nff = "nff";
-	    public final static String wimp = "wimp";
-	    public final static String txt = "txt";
-	    public final static String ply = "ply";
-	    
-	    /*
-	     * Get the extension of a file.
-	     */  
-	    public static String getExtension(File f) {
-	        String ext = null;
-	        String s = f.getName();
-	        int i = s.lastIndexOf('.');
+		public final static String nff = "nff";
+		public final static String wimp = "wimp";
+		public final static String txt = "txt";
+		public final static String ply = "ply";
 
-	        if (i > 0 &&  i < s.length() - 1) {
-	            ext = s.substring(i+1).toLowerCase();
-	        }
-	        return ext;
-	    }
+		/*
+		 * Get the extension of a file.
+		 */
+		public static String getExtension(File f) {
+			String ext = null;
+			String s = f.getName();
+			int i = s.lastIndexOf('.');
+
+			if (i > 0 && i < s.length() - 1) {
+				ext = s.substring(i + 1).toLowerCase();
+			}
+			return ext;
+		}
 
 		public static boolean accept(File f) {
 			String extension = Utils.getExtension(f);
